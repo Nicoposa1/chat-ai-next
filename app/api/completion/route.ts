0bb133e0ddd6860e3f59008d56d6adb25efa3538
Nicolas Posa <nicoposa57@gmail.com>
2023-11-07 01:35:54 -0300

    first commit

---
+++ app/api/completion/route.ts
@@ -0,0 +1,11 @@
+export async function GET(request: Request) {
+  return new Response(
+    JSON.stringify({
+      message: "Hello World"
+    }), {
+      headers: {
+        "content-type": "application/json;charset=UTF-8"
+      }
+    }
+  )
+}
